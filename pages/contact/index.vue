0641ad9fd7061db6b49f014ee5102527b67e6139
chatthong <chatthong_hon@jobbkk.co.th>
2020-07-05 17:10:58 +0700

    Initial Commit

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,29 +1,13 @@
 <template>
   <v-col cols="12">
-    <input class="input" ref="searchTextField" type="text" />
+
   </v-col>
 </template>
 
 <script>
-const apiKey = process.env.GOOGLE_MAPS_API_KEY; // Package: @nuxtjs/dotenv
+
 export default {
-  head() {
-    return {
-      script: [
-        {
-          src: `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`
-        }
-      ]
-    };
-  },
-  methods: {},
-  mounted() {
-    var input = this.$refs.searchTextField; //.getElementById('searchTextField');
-    new google.maps.places.Autocomplete(input);
-  },
-  data() {
-    return {};
-  }
+
 };
 </script>
 
