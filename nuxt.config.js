19983fb89095f9789e3dacfd68fa873f425e9364
chatthong <chatthong_hon@jobbkk.co.th>
2020-07-13 07:23:22 +0700

    Initial Commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
-  mode: "universal",
-  // mode: "spa",
-  // router: {
-  //   base: "/portfolioNuxt/"
-  // },
+  // mode: "universal",
+  mode: "spa",
+  router: {
+    base: "/portfolioNuxt/"
+  },
   generate: {
     fallback: true, // if you want to use '404.html' instead of the default '200.html'
   },
